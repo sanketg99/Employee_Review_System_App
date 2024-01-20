6bc947ab2c68871dc5fdc99d89a4cb1662320a89
sanketg99 <sanketgorade99@gmail.com>
2024-01-20 19:47:05 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://sanketbg99:sanket123@cluster0.ewcrs64.mongodb.net/?retryWrites=true&w=majority/`);
+mongoose.connect(`mongodb+srv://sanketbg99:sanket123@cluster0.ewcrs64.mongodb.net/?retryWrites=true&w=majority`);
 //mongodb://localhost:27017/employeeReview
-// mongodb+srv://sanketbg99:sanket123@cluster0.ewcrs64.mongodb.net/?retryWrites=true&w=majority/
+// mongodb+srv://sanketbg99:sanket123@cluster0.ewcrs64.mongodb.net/?retryWrites=true&w=majority
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
